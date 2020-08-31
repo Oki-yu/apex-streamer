7789c57cdf3b2db9e489a27f8a2dd5609b4dfdb2
Oki-yu <radsolars@yahoo.co.jp>
2020-08-31 13:13:40 +0900

    add tewwt

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'streams#index'
   
   resources :streams, only: [:index]
+  resources :tweets
   
   resources :messages
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_051829) do
+ActiveRecord::Schema.define(version: 2020_08_31_040511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "text"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
